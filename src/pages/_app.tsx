332e74998da6df92392184413a4659646c335751
Daniela Vornic <daniela.vornic@gmail.com>
2023-09-19 00:34:13 +0300

    init repo with necessary packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// TODO: Add theme colors
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      900: "#1a202c",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />{" "}
+    </ChakraProvider>
+  );
 }

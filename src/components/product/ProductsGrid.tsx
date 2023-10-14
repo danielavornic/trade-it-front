285dc85ad7bfa6029bb28584a0af7ca88d3c13fd
Daniela Vornic <daniela.vornic@gmail.com>
2023-10-15 01:21:20 +0300

    Remove unused code in ProductsGrid

--- src/components/product/ProductsGrid.tsx
+++ src/components/product/ProductsGrid.tsx
@@ -1,24 +1,11 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import React from "react";
-import Slider from "react-slick";
 import { Grid, GridItem } from "@chakra-ui/react";
-import { Product } from "@/types";
 import { ProductGridCard } from "./ProductGridCard";
 
-export const ProductsGrid = ({ products }: { products?: Product[] }) => {
+export const ProductsGrid = ({ products }: { products?: any[] }) => {
   if (!products || products.length === 0) {
     return null;
   }
-
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    arrows: true,
-  };
 
   return (
     <Grid templateColumns="repeat(4, 1fr)" gap={6}>

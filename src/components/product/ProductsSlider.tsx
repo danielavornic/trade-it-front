84b6b913923b3328cb9636c773f45ac1037ec784
Daniela Vornic <daniela.vornic@gmail.com>
2023-10-15 01:21:04 +0300

    Add Product Slider, cta and info section in homepage

--- src/components/product/ProductsSlider.tsx
+++ src/components/product/ProductsSlider.tsx
@@ -1,7 +1,51 @@
-export const ProductsSlider = ({ products }: { products?: any[] }) => {
+import { Box, Flex, Heading, VStack, Icon } from "@chakra-ui/react";
+import { BsStars } from "react-icons/bs";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { ProductGridCard } from "@/components";
+
+export const ProductsSlider = ({ title, products }: { title: string; products?: any[] }) => {
   if (!products) {
     return null;
   }
 
-  return <div>ProductsSlider</div>;
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  };
+
+  return (
+    <Flex w="100%" alignItems="stretch">
+      <VStack
+        justifyContent="center"
+        alignItems="center"
+        bg="accent.500"
+        w="25%"
+        borderRadius="lg"
+        spacing={10}
+        color="white"
+      >
+        <Icon as={BsStars} boxSize={16} />
+        <Heading as="h2" size="2xl" textAlign="center" lineHeight="1.33">
+          {title}
+        </Heading>
+      </VStack>
+      <Box w="75%">
+        <Slider {...settings}>
+          {products.map((product, index) => (
+            <Flex px={4} key={index} h="full" transform="translateY(-4px)">
+              <ProductGridCard product={product} />
+            </Flex>
+          ))}
+        </Slider>
+      </Box>
+    </Flex>
+  );
 };

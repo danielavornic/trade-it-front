c60eb9b28b693a36189e04b48626d8aee6c28d56
maria <118808220+yum3lo@users.noreply.github.com>
2023-10-18 21:26:52 +0300

    seller card 1st commit

--- src/components/product/SellerCard.tsx
+++ src/components/product/SellerCard.tsx
@@ -5,12 +5,35 @@
 // https://chakra-ui.com/docs/components/button/usage
 // https://chakra-ui.com/docs/components/text/usage
 
-import { Card, Button, Stack, StackDivider, Text, Avatar } from "@chakra-ui/react";
+import { Card, Button, Stack, StackDivider, Text, Avatar, Box } from "@chakra-ui/react";
+import { FaMapMarkerAlt, FaHeart } from "react-icons/fa";
 
 export const SellerCard = ({ sellerUsername }: { sellerUsername: string }) => {
   // vedem de unde luam asa info de la back
   // textul cu verified seller lasam asa de forma
   const location = "Chișinău, MD";
 
-  return <Card>SellerCard</Card>;
+  return (
+    <Card>
+      <Stack spacing={4}>
+        <Avatar bg="#0EB085">
+          {/*avatar content here*/}
+        </Avatar>
+        <Text>{sellerUsername}</Text>
+        <Stack direction="row" spacing={2}>
+          <Box as={FaMapMarkerAlt} fontSize="lg" color="gray.500" />
+          <Text>{location}</Text>
+        </Stack>
+        <Button colorScheme="#0EB085" variant="solid">
+          Initiate Barter
+        </Button>
+        <Stack direction="row" spacing={2}>
+          <Box as={FaHeart} fontSize="lg" color="#0EB085" />
+          <Button colorScheme="gray" variant="outline" color="#0EB085">
+            Save for Later
+          </Button>
+        </Stack>
+      </Stack>
+    </Card>
+  );
 };

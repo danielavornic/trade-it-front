6ff0be0f5aac8e70ac544f9ae1d36c393a6ca8da
Daniela Vornic <daniela.vornic@gmail.com>
2023-10-23 14:18:19 +0300

    Add next progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NextProgress from "next-progress";
 
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
@@ -36,10 +39,25 @@ const theme = extendTheme({
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.scrollTo({ top: 0 });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <AuthProvider>
+          <NextProgress options={{ showSpinner: true }} color="#0EB085" height={4} />
           <Component {...pageProps} />
         </AuthProvider>
       </ChakraProvider>

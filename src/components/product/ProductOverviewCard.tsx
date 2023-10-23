057f39365060a4d49c7e8b6a1d7bbba6c277e7c0
Daniela Vornic <daniela.vornic@gmail.com>
2023-10-23 12:49:28 +0300

    Update elements order in products overview card

--- src/components/product/ProductOverviewCard.tsx
+++ src/components/product/ProductOverviewCard.tsx
@@ -1,4 +1,4 @@
-import { Card, HStack, Image, Box, Text, VStack, Spacer } from "@chakra-ui/react";
+import { Card, HStack, Image, Box, Text, VStack, Spacer, Heading, Badge } from "@chakra-ui/react";
 import { Product } from "@/types";
 import { SellerCard } from "./SellerCard";
 
@@ -6,8 +6,8 @@ export const ProductOverviewCard = ({ product }: { product: Product }) => {
   const { name, description, img, condition, targetProducts } = product;
 
   return (
-    <Card shadow="none" p={2} fontSize="3xl">
-      <HStack justify="space-between">
+    <Card shadow="none" p={2} fontSize="3xl" border="1px solid" borderColor="gray.200">
+      <HStack justify="space-between" alignItems="flex-start" spacing={8}>
         <Image
           src={img}
           alt={name}
@@ -16,17 +16,26 @@ export const ProductOverviewCard = ({ product }: { product: Product }) => {
           height="400px"
           objectFit="cover"
           borderRadius="md"
+          border="1px solid"
+          borderColor="gray.200"
         />
         <VStack p={2} align="start" spacing={2}>
           <Box padding={2}>
-            <Text fontSize="3xl" fontWeight="bold">
+            <Badge fontSize="0.5em" mb="6">
+              {condition}
+            </Badge>
+            <Heading fontSize="4xl" as="h1" fontWeight="bold" mb="4">
               {name}
+            </Heading>
+            <Text fontSize="xl" mb="4">
+              <Text color="brand.500" display="inline" fontWeight="bold">
+                Looking for:{" "}
+              </Text>
+              {targetProducts}
             </Text>
-            <Text fontSize="xl">{description}</Text>
-            <Text fontSize="xl" color="gray.500">
-              Condition: {condition}
+            <Text fontSize="lg" color="gray.700">
+              {description}
             </Text>
-            <Text fontSize="xl">Target Products: {targetProducts}</Text>
           </Box>
         </VStack>
         <Spacer />

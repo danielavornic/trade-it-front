b1c673bae9870edf88a911b30e9bc57be94133b6
Daniela Vornic <daniela.vornic@gmail.com>
2023-11-23 15:26:12 +0200

    Mock authentication

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+var jwt = require("jsonwebtoken");
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const obj = { ...req.body, user_id: 1 };
+    const token = jwt.sign(obj, "shhhhh");
+    return res.status(200).json({ token });
+  }
+}
